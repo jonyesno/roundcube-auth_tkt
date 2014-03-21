206b17bc2bd5c51b88834178b77581092bb5d708
Jon Stuart <jon@zomo.co.uk>
2014-03-21 10:47:17 +0700

    Bump version

--- auth_tkt.php
+++ auth_tkt.php
@@ -11,7 +11,7 @@
  *
  * See https://github.com/gavincarr/mod_auth_tkt for more about mod_auth_tkt
  *
- * @version 0.1.0
+ * @version 0.1.1
  * @author Jon Stuart
  * @url https://github.com/zomo/roundcube-auth_tkt
  */
